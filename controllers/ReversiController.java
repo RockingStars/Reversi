c22c2060cee449999f458af980459ba24e10a9cb
Robert Monden <dev@robertmonden.com>
2018-04-17 08:32:58 +0200

    Javadoc

--- controllers/ReversiController.java
+++ controllers/ReversiController.java
@@ -32,6 +32,7 @@ import com.rockingstar.engine.gui.controllers.AudioPlayer;
 import com.rockingstar.engine.io.models.Util;
 import com.rockingstar.modules.Reversi.models.ReversiModel;
 import com.rockingstar.modules.Reversi.views.ReversiView;
+
 import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
@@ -40,12 +41,33 @@ import javafx.scene.control.ButtonType;
 import java.util.ArrayList;
 import java.util.Optional;
 
+/**
+ * This class sets up Reversi
+ * @author Rocking Stars
+ * @since 1.0 Beta 1
+ */
 public class ReversiController extends AbstractGame {
 
+    /**
+     * The model contains basic reversi algorithms, used for determining if we have a winner
+     */
     private ReversiModel _model;
+
+    /**
+     * Stores the graphical board, among other things
+     */
     private ReversiView _view;
+
+    /**
+     * An instance of the AudioPlayer, containing the background music.
+     */
     private AudioPlayer _backgroundMusic;
 
+    /**
+     * ReversiController constructor.
+     * @param player1 The local player
+     * @param player2 The opponent
+     */
     public ReversiController(Player player1, Player player2) {
         super(player1, player2);
 
@@ -70,12 +92,20 @@ public class ReversiController extends AbstractGame {
             ((EasyAI) player1).setModel(_model);
     }
 
+    /**
+     * Returns the view
+     * @return The view
+     */
     @Override
     public Node getView() {
         return _view.getNode();
-
     }
 
+    /**
+     * Handles player moves by the local player (after clicking on a tile).
+     * @param x The x position
+     * @param y The y position
+     */
     @Override
     public void doPlayerMove(int x, int y) {
         _model.clearPossibleMoves();
@@ -101,6 +131,10 @@ public class ReversiController extends AbstractGame {
         }
     }
 
+    /**
+     * Handles moves by external players.
+     * @param position The position
+     */
     @Override
     public void doPlayerMove(int position) {
         Platform.runLater(() -> getScores());
@@ -127,6 +161,10 @@ public class ReversiController extends AbstractGame {
         }
     }
 
+    /**
+     * Makes sure the player is able to make a move. When the local player is an AI, a move is
+     * automatically sent back in this method.
+     */
     @Override
     public void doYourTurn () {
         yourTurn = true;
@@ -153,6 +191,10 @@ public class ReversiController extends AbstractGame {
         }
     }
 
+    /**
+     * Ends the game with a certain result.
+     * @param result The result of the game (WIN< LOSS or DRAW)
+     */
     @Override
     public void gameEnded (String result){
         Platform.runLater(() -> getScores());
@@ -187,7 +229,10 @@ public class ReversiController extends AbstractGame {
 
     }
 
-
+    /**
+     * Sets the starting player
+     * @param player The starting player
+     */
     public void setStartingPlayer(Player player) {
         player1.setCharacter(player.getUsername().equals(player1.getUsername()) ? 'b' : 'w');
         player2.setCharacter(player1.getCharacter() == 'b' ? 'w' : 'b');
@@ -202,7 +247,10 @@ public class ReversiController extends AbstractGame {
         _view.updatePlayerColors();
     }
 
-    public void addEventHandlers() {
+    /**
+     * Adds event handlers to the view
+     */
+    private void addEventHandlers() {
         _view.getEndButton().setOnAction(e ->{
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Closing game");
@@ -218,32 +266,58 @@ public class ReversiController extends AbstractGame {
         _view.getRageQuitButton().setOnAction(e -> Platform.exit());
     }
 
-    public void getScores(){
+    /**
+     * Retrieves the scores of both players and adds them to the view
+     */
+    private void getScores() {
         int[] scores = _model.getScore();
         _view.getP1Score().setText("" + scores[player1.getCharacter() == 'b' ? 0 : 1]);
         _view.getP2Score().setText("" + scores[player2.getCharacter() == 'b' ? 0 : 1]);
     }
 
+    /**
+     * Sets up the background music
+     */
     private void setupBackgroundMusic() {
         _backgroundMusic = new AudioPlayer("ReversiMusic.mp3", true);
     }
-  
+
+    /**
+     * Returns the username of player 1
+     * @return The username of player 1
+     */
     public String getPlayer1Name(){
         return player1.getUsername();
     }
 
+    /**
+     * Returns the username of player 2
+     * @return The username of player 2
+     */
     public String getPlayer2Name(){
         return player2.getUsername();
     }
 
+    /**
+     * Returns the character (not color) of player 1.
+     * @return Player 1's character
+     */
     public char getColorP1(){
         return player1.getCharacter();
     }
 
+    /**
+     * Returns a Player object of the local player
+     * @return The local player
+     */
     public Player getPlayer1(){
         return player1;
     }
 
+    /**
+     * Returns a Player objects of the external player
+     * @return The external player
+     */
     public Player getPlayer2(){
         return player2;
     }
